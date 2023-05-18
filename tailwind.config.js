94ebcb8e3803c36603258d188fac4a132a10c604
kelvinpraises <kelvinpraises@gmail.com>
2023-05-18 21:39:56 +0100

    feat: add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: "#80C1F9",
+          200: "#40A3F7",
+          300: "#0084F4",
+        },
+        text: {
+          primary: "#999999",
+          secondary: "#D9D9D9",
+        },
+      },
+      backgroundColor: {
+        search: "#212226",
+        nav: "#212226",
+        body: "#18191D",
+        card: "#1E1F23",
+        profile: "#41464F",
       },
     },
   },
   plugins: [],
-}
+};
