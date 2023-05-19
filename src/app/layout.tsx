6a03b8739e7b90436862ea019576ba9b61a1566c
kelvinpraises <kelvinpraises@gmail.com>
2023-05-19 14:43:05 +0100

    feat: add montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 import Nav from "@/components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 export const metadata = {
   title: "SoundSphere",
-  description: "Join or create a music label DAO and earn royalties from each streamed music in the DAO dataset",
+  description:
+    "Join or create a music label DAO and earn royalties from each streamed music in the DAO dataset",
 };
 
 export default function RootLayout({
@@ -16,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={"flex h-full" + inter.className}>
+    <html className={`${montserrat.variable}`} lang="en">
+      <body className={"flex h-full"}>
         <Nav />
         <main className="flex gap-12 min-h-screen w-full flex-col items-center p-8 px-24 overflow-scroll">
           {children}
