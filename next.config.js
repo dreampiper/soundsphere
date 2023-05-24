a7dd1d82444f8935ce2e47fe0835c14b708b8ffa
kelvinpraises <kelvinpraises@gmail.com>
2023-05-24 06:42:22 +0100

    feat: add remote patterns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
